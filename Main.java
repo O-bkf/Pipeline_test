95f386169b7f75735f2b6c7838a31b15733714c2
omar <bounekhataoumar@gmail.com>
2023-02-02 15:29:16 +0000

    second commit

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello word, how are you?");
+        System.out.println("Hello word,ARE YOU FINE");
     }
 }
