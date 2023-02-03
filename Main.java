ed9a5cff1aa7b0e1d3197d0edc1164a22aa5eaa9
omar <bounekhataoumar@gmail.com>
2023-02-03 12:12:27 +0000

    hello brother

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String args[]){
-        System.out.println("Hello are you fine my brother");
+        System.out.println("Hello my brother");
     }
 }
